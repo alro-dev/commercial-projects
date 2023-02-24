60ede63e8410584ae4c061cd359c1eb6de1f6878
alro-dev <alarogala@gmail.com>
2023-02-24 11:02:32 +0100

    Bike Life- new project and rwd - menu tests

--- element/e2e/cypress/e2e/element-homepage.cy.js
+++ element/e2e/cypress/e2e/element-homepage.cy.js
@@ -1,5 +1,22 @@
+import 'cypress-each'
+var adresPage = 'https://reklama-element.pl/'
+const menuItems = [
+    ['http://reklama-element.pl/#services', 'Usługi'],
+    ['http://reklama-element.pl/kontakt/', 'Kontakt'],
+    ['http://reklama-element.pl/#testimonials','Klienci'],
+    ['http://reklama-element.pl/portfolio/','Projekty'],
+]
+
 describe('Home Page', () => {
   it('page is loaded', () => {
-    cy.visit('http://reklama-element.pl')
+    var rwdPage = 'https://reklama-element.pl/'
+    cy.visit(adresPage);
+    cy.url().should('include', adresPage);
+    });
+
+    it.each(menuItems)('%s page has %s header', (href, text) => {
+      var rwdPage = 'https://reklama-element.pl/';
+      cy.visit(adresPage);
+      cy.get("[href='"+ href + "']").click(); //"a.nav-link[href='adresPage/#services']"
+    })
   })
-})
