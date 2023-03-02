e27146f690a717f9eef06ca5d6b1afeb3d2c1f1f
alro-dev <alarogala@gmail.com>
2023-03-02 16:23:34 +0100

    Element - Home Page menu and facebook tests

--- element/e2e/cypress/e2e/element-homepage.cy.js
+++ element/e2e/cypress/e2e/element-homepage.cy.js
@@ -1,22 +1,41 @@
 import 'cypress-each'
 var adresPage = 'https://reklama-element.pl/'
 const menuItems = [
-    ['http://reklama-element.pl/#services', 'Usługi'],
-    ['http://reklama-element.pl/kontakt/', 'Kontakt'],
-    ['http://reklama-element.pl/#testimonials','Klienci'],
-    ['http://reklama-element.pl/portfolio/','Projekty'],
+  ['menu-item-853', 'Usługi'],
+  ['menu-item-30', 'Kontakt'],
+  ['menu-item-854','Klienci'],
+  ['menu-item-31','Projekty'],
 ]
 
 describe('Home Page', () => {
   it('page is loaded', () => {
-    var rwdPage = 'https://reklama-element.pl/'
+    var adresPage = 'https://reklama-element.pl/'
+    cy.visit(adresPage);
+    cy.url().should('include', adresPage);
+  });
+
+    it.each(menuItems)('%s page has %s header', (id, text) => {
+      var adresPage = 'https://reklama-element.pl/';
+      cy.visit(adresPage);
+      cy.get("#" + id).click({force: true}); //"#menu-item-853"
+    })
+})
+
+const menuIcons = [
+  ['https://www.facebook.com/Element-Warsztat-Reklamy-106973381465824', 'facebook'],
+]
+
+describe('Home Page - menuFacebookOpen', () => {
+  it('page is loaded', () => {
+    var adresPage = 'https://reklama-element.pl/';
     cy.visit(adresPage);
     cy.url().should('include', adresPage);
     });
-
-    it.each(menuItems)('%s page has %s header', (href, text) => {
-      var rwdPage = 'https://reklama-element.pl/';
-      cy.visit(adresPage);
-      cy.get("[href='"+ href + "']").click(); //"[href='adresPage/#services']"
-    })
-  })
+    
+    it.each(menuIcons)('%s page has %s menu', (href, text) => {
+        var adresPage = 'https://reklama-element.pl/';
+        cy.visit(adresPage);
+        cy.get("[href='"+ href + "']").click({force: true, multiple: true}); //"menu-icons[href='facbook']"
+      });
+        
+})
